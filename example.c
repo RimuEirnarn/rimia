e8d5866fc646b24315500d693f91eafc2243c9b5
RimuEirnarn <Rimuru720@proton.me>
2025-12-16 14:50:49 +0700

    Added proper foreach and rename old foreach to for_

--- example.c
+++ example.c
@@ -5,18 +5,21 @@
 DEFINE_DA(int, IntArray);
 
 int main() {
-    IntArray arr = IntArray_init();
+    //IntArray arr = IntArray_init();
+    DA_INIT(IntArray, arr);
     arr.push(&arr, 67);
     arr.push(&arr, 69);
 
     frange(50) {
         arr.push(&arr, i);
     }
+
     printf("Total: %ld\n", arr.count);
 
-    foreach_varda(itercount, arr) {
-        printf("%zu -> %d\n", itercount, arr.data[itercount]);
+    foreach_vida(arr, index, value) {
+        printf("%zu -> %d\n", index, value);
     }
 
     arr.pop(&arr);
+    arr.free(&arr);
 }
--- rimia_da.h
+++ rimia_da.h
@@ -3,6 +3,7 @@
 #include "rimia_err.h"
 #define RIMIA_DA_H
 
+// Define dynamic array by type to name
 #define DEFINE_DA(TYPE, NAME) \
     typedef struct NAME NAME; \
     struct NAME { \
@@ -42,7 +43,29 @@
         return self; \
     } \
 
-#define foreach_varda(var, self) for (size_t var = 0; (var) < self.count; ++(var))
-#define foreach_da(self) for (size_t i = 0; i < (self).count; i++)
+// Initialize dynamic array so you don't need to pass Type_init().
+#define DA_INIT(type, name) type name = type##_init()
+
+// For-loop of the dynamic array. Pass your variable name and the instance.
+#define for_varda(var, self) for (size_t var = 0; (var) < self.count; ++(var))
+// For-loop of the dynamic array. It uses "i" var. Don't foret to pass the instance.
+#define for_da(self) for_varda(i)
+
+// Dynamic Array Foreach. Pass the instance, index name, var name.
+#define foreach_vida(self, idx, name) \
+    for (size_t (idx) = 0; (idx) < (self).count; (idx)++) \
+        for (int _rda_once = 1; _rda_once; _rda_once = 0)\
+        for (__typeof__((self).data[(idx)]) (name) = (self).data[(idx)]; _rda_once; _rda_once = 0)
+
+// Dynamic Array Foreach. Only need the instance. Uses "i" and "val".
+#define foreach_da(self) foreach_vida(self, i, val)
+// Dynamic Array Foreach. Only need the instance and var name. Uses "i".
+#define foreach_vda(self, name) foreach_vida(self, i, name)
+
+// Dynamic Array Foreach BY reference. Only need the instance and var name. Uses "_i".
+#define foreach_vda_ref(arr, var)\
+    for (size_t _i = 0; _i < (arr).count; ++_i)\
+        for (int _once = 1; _once; _once = 0)\
+            for (__typeof__((self).data[i]) *var = &(arr).data[_i]; _once; _once = 0)
 
 #endif
